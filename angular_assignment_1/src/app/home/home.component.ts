f4c9efa2d6ba9b98e384f542c80813d24e4669b8
Dipa Majumdar <DipaMajumdar@promact.com>
2018-02-05 18:37:43 +0530

    second commit

--- angular_assignment_1/src/app/home/home.component.ts
+++ angular_assignment_1/src/app/home/home.component.ts
@@ -8,40 +8,46 @@ import {Employee} from '../models/employee.model';
 export class HomeComponent implements OnInit  {
  	experiences : number[];
   qualifications: string[];
-  coding_languages: string[];
+  coding_languages: string[];  
  new_coding_languages: string[];
+
  //new_coding_languages=[];
-						chk_lang(lang: string)
+						
+
+
+
+ ngOnInit(): void{
+  this.qualifications=['Graduation','under-graduation','post-graduation'];
+  this.experiences=[1,2,3,4,5];
+  this.coding_languages=['C/C++','Java','C#','Python','Ruby'];
+  this.new_coding_languages=[''];
+
+ }
+
+  model = new Employee('','','','','','','', '',this.new_coding_languages);
+ chk_lang(lang: string)
             { 
               var flag=0;
                 for(var i=0;i<5;i++)
                 {
                       if(this.new_coding_languages[i]==lang)
                          {
-                         
                          flag=1;
                          break;
                          }
                 }
-                if(flag==1){this.new_coding_languages.splice(i,1);
+                if(flag==1)
+                {
+                  this.new_coding_languages.splice(i,1);
                 }
                 else
                 {this.new_coding_languages.push(lang);}
-      // console.log(this.new_coding_languages);
+               // console.log();
             }
-
- model = new Employee('','','','','','','', '',this.new_coding_languages);
-
- ngOnInit(): void{
-  this.qualifications=['Graduation','under-graduation','post-graduation'];
-  this.experiences=[1,2,3,4,5];
-   this.coding_languages=['C/C++','Java','C#','Python','Ruby'];
-  this.new_coding_languages=[];
-
- }
 onSubmit()
 {
     console.log(this.model);
+    console.log(this.new_coding_languages);
 }
 
 
